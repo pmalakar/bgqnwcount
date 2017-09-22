f00d5230eb60be6bf94d99d295601dd6b3186e08
Preeti Malakar <preeti.malakar@gmail.com>
2017-09-22 19:42:57 +0000

    tabs to spaces

--- multisend.cxx
+++ multisend.cxx
@@ -17,7 +17,7 @@ int main (int argc, char **argv) {
   int result;
   double tStart, tEnd; 
 
-    MPI_Status status;
+  MPI_Status status;
   MPI_Request *send_request, *recv_request;
   MPI_Status *send_status, *recv_status;
 
@@ -30,13 +30,13 @@ int main (int argc, char **argv) {
   MPI_Comm_rank (MPI_COMM_WORLD, &myrank);
   MPI_Comm_size (MPI_COMM_WORLD, &commsize);
 
-    bgpminit();
+  bgpminit();
 
-    send_status = (MPI_Status *) malloc(commsize/2 * sizeof(MPI_Status));
-    recv_status = (MPI_Status *) malloc(commsize/2 * sizeof(MPI_Status));
+  send_status = (MPI_Status *) malloc(commsize/2 * sizeof(MPI_Status));
+  recv_status = (MPI_Status *) malloc(commsize/2 * sizeof(MPI_Status));
 
-    send_request = (MPI_Request *) malloc(commsize/2 * sizeof(MPI_Request));
-    recv_request = (MPI_Request *) malloc(commsize/2 * sizeof(MPI_Request));
+  send_request = (MPI_Request *) malloc(commsize/2 * sizeof(MPI_Request));
+  recv_request = (MPI_Request *) malloc(commsize/2 * sizeof(MPI_Request));
 
   dataBlock *datum = new dataBlock(count);   //initializes alpha array to random double values
 
@@ -47,26 +47,26 @@ int main (int argc, char **argv) {
 
   tStart = MPI_Wtime();
 
-    int j=-1;
-    for (int dest=(myrank+1)%commsize ; dest!=(myrank+1+commsize/2)%commsize ; dest=(dest+1)%commsize) {
-   ++j;
-   result = MPI_Isend (datum->getAlphaBuffer(), count, MPI_DOUBLE, dest, myrank+1, MPI_COMM_WORLD, &send_request[j]);
+  int j=-1;
+  for (int dest=(myrank+1)%commsize ; dest!=(myrank+1+commsize/2)%commsize ; dest=(dest+1)%commsize) {
+    ++j;
+    result = MPI_Isend (datum->getAlphaBuffer(), count, MPI_DOUBLE, dest, myrank+1, MPI_COMM_WORLD, &send_request[j]);
   }
 
-    int k=-1;
-    for (int source=(myrank+commsize/2)%commsize ; source!=myrank ; source=(source+1)%commsize) {
+  int k=-1;
+  for (int source=(myrank+commsize/2)%commsize ; source!=myrank ; source=(source+1)%commsize) {
     ++k;
-   MPI_Recv (datum->getAlphaBuffer(), count, MPI_DOUBLE, source, source+1, MPI_COMM_WORLD, &status);
+    MPI_Recv (datum->getAlphaBuffer(), count, MPI_DOUBLE, source, source+1, MPI_COMM_WORLD, &status);
   }
   tEnd = MPI_Wtime();
 
-   MPI_Waitall(commsize/2, send_request, send_status);
+  MPI_Waitall(commsize/2, send_request, send_status);
 
   if (myrank == 0)
-  printf("Perf %d %d %lf\n", commsize, count, tEnd-tStart);
+    printf("Perf %d %d %lf\n", commsize, count, tEnd-tStart);
   
-    bgpmfinalize();
-    PrintCounts("Network", hNWSet, myrank);
+  bgpmfinalize();
+  PrintCounts("Network", hNWSet, myrank);
 
   MPI_Finalize ();
 
